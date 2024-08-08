64486ccb5e3856d6bfad45b7b1c1b21badf30567
joaopaulo005 <jpcostadossantos05@gmail.com>
2024-08-08 20:28:37 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from unidecode import unidecode
+
+def rodar_quiz():
+    #lista de perguntas e respostas
+    perguntas = [
+        {'pergunta': 'Qual time tem o escudo mais bonito do mundo?', 'resposta': 'Botafogo'},
+        {'pergunta': 'Qual o maior planeta do sistema solar?', 'resposta': 'Júpiter'},
+        {'pergunta': 'Quantos continentes existem no mundo?', 'resposta': '7'},
+        {'pergunta': 'Quem pintou a Mona Lisa?', 'resposta': 'Leonardo da Vinci'},
+        {'pergunta': 'Qual o elemento químico representado pelo símbolo "O"?', 'resposta': 'Oxigênio'},
+         {'pergunta': 'Qual é o menor país do mundo?', 'resposta': 'Vaticano'},
+        {'pergunta': 'Em que ano começou a Segunda Guerra Mundial?', 'resposta': '1939'},
+        {'pergunta': 'Qual é o maior oceano da Terra?', 'resposta': 'Oceano Pacífico'},
+        {'pergunta': 'Quantos graus tem um ângulo reto?', 'resposta': '90'},
+        {'pergunta': 'Qual é o país mais populoso do mundo?', 'resposta': 'China'},
+    ]
+     
+    #variavel para armazenar a pontuação
+    pontuacao = 0
+
+    for item in perguntas:
+        print(item['pergunta'])
+        resposta_player = input('Sua resposta: ')
+
+    #verificar a resposta
+        if unidecode(resposta_player.upper()) == unidecode(item['resposta'].upper()):
+            print('Resposta Correta!\n')
+            pontuacao += 1
+        else:
+            print(f'Resposta errada. O correto é: {item["resposta"]}\n')
+
+    print(f'Você acertou {pontuacao} de {len(perguntas)} perguntas!')
+
+rodar_quiz()
